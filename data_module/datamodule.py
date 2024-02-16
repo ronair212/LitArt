e792ee27c46aee85681e52dc8f580f52d7efbdd5
shivanshvermaa <shivansh.verma8080@gmail.com>
2024-02-16 13:08:52 -0500

    Changes made for textmodules

--- data_module/datamodule.py
+++ data_module/datamodule.py
@@ -7,46 +7,6 @@ from data_preprocessor import TextPreprocessing
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-
-
-class TextSummaryDataset(Dataset):
-    def __init__(self,
-                 df,
-                 textprocessor,
-                 tokenizer,
-                 tokenizer_chapter_max_length=1024,
-                 tokenizer_summary_max_length=64,
-                 truncation=True,
-                 ):
-
-        self.df = df
-        self.textprocessor = textprocessor
-        self.chapter = df["chapter"]
-        self.summary = df["summary_text"]
-        self.tokenizer = tokenizer
-        self.tokenizer_chapter_max_length = tokenizer_chapter_max_length
-        self.tokenizer_summary_max_length = tokenizer_summary_max_length
-        self.truncation = truncation
-
-    def __len__(self):
-        return len(self.chapter)
-
-    def __getitem__(self,idx):
-        chapter = "summarize:" + str(self.textprocessor.process(self.chapter[idx]))
-        summary = self.textprocessor.process(self.summary[idx])
-
-        input_encodings = self.tokenizer(chapter, max_length=self.tokenizer_chapter_max_length,padding="max_length", truncation=self.truncation)
-
-        with self.tokenizer.as_target_tokenizer():
-            target_encodings = self.tokenizer(summary, max_length=self.tokenizer_summary_max_length,padding="max_length", truncation=self.truncation)
-
-        return {
-            "input_ids": torch.tensor(input_encodings["input_ids"], dtype=torch.long),
-            "attention_mask": torch.tensor(input_encodings["attention_mask"], dtype=torch.long),
-            "labels": torch.tensor(target_encodings["input_ids"], dtype=torch.long),
-            "summary_mask": torch.tensor(target_encodings["attention_mask"], dtype=torch.long)
-        }
-
 class TextDataModule(L.LightningDataModule):
     def __init__(self,
                  train_path,
@@ -62,23 +22,15 @@ class TextDataModule(L.LightningDataModule):
 
         super().__init__()
 
-        # Reading the train file
-        try:
-            self.train_df = pd.read_csv(train_path)
-        except Exception as e:
-            print(f"Exception raised while reading training file at path : {train_path} \n Exception : {e}")
+        # Initializing Paths
+        self.train_path = train_path
+        self.test_path = test_path
+        self.val_path = val_path
 
-        # Reading the test file
-        try:
-            self.test_df = pd.read_csv(test_path)
-        except Exception as e:
-            print(f"Exception raised while reading test file at path : {test_path} \n Exception : {e}")
-
-        # Reading the validation file
-        try:
-            self.val_df = pd.read_csv(val_path)
-        except Exception as e:
-            print(f"Exception raised while reading validation file at path : {val_path} \n Exception : {e}")
+        # Initializing Dataframes
+        self.train_df = None
+        self.test_df = None
+        self.val_df = None
 
         # Textprocessor setup
         self.textprocessor = textprocessor
@@ -91,6 +43,25 @@ class TextDataModule(L.LightningDataModule):
 
         # Batch size setup
         self.batch_size = batch_size
+
+    def prepare_data(self):
+         # Reading the train file
+        try:
+            self.train_df = pd.read_csv(self.train_path)
+        except Exception as e:
+            print(f"Exception raised while reading training file at path : {self.train_path} \n Exception : {e}")
+
+        # Reading the test file
+        try:
+            self.test_df = pd.read_csv(self.test_path)
+        except Exception as e:
+            print(f"Exception raised while reading test file at path : {self.test_path} \n Exception : {e}")
+
+        # Reading the validation file
+        try:
+            self.val_df = pd.read_csv(self.val_path)
+        except Exception as e:
+            print(f"Exception raised while reading validation file at path : {self.val_path} \n Exception : {e}")
 
 
     def setup(self, stage= None):
@@ -162,32 +133,6 @@ class ImageDataModule(L.LightningModule):
 
     def predict_dataloader(self):
         return DataLoader(self.predict, batch_size=self.batch_size)
-
-class ImageDataModule(L.LightningModule):
-    def __init__(self, data_dir: str = "path/to/dir", batch_size: int = 32):
-        super().__init__()
-        self.data_dir = data_dir
-        self.batch_size = batch_size
-
-    def prepare_data(self):
-        pass
-
-    def setup(self, stage: str):
-        ## Image Data
-        pass
-
-    def train_dataloader(self):
-        return DataLoader(self.traindataset, batch_size=self.batch_size)
-
-    def val_dataloader(self):
-        return DataLoader(self.valdataset, batch_size=self.batch_size)
-
-    def test_dataloader(self):
-        return DataLoader(self.testdataset, batch_size=self.batch_size)
-
-    def predict_dataloader(self):
-        return DataLoader(self.predict, batch_size=self.batch_size)
-
 
 
 # # Training files setup
