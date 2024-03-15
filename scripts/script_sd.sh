b0f9ad968cda2f51e6a2612adf9943534e8fe025
adupatil <adwaitpatil1199@gmail.com>
2024-03-15 12:31:25 -0400

    shell script updated

--- scripts/script_sd.sh
+++ scripts/script_sd.sh
@@ -22,18 +22,18 @@
 
 export MODEL_NAME="CompVis/stable-diffusion-v1-4"
 export TRAIN_DIR="../data/"
-export OUTPUT_DIR="./trained_models/"
+export OUTPUT_DIR="/work/LitArt/adwait/capstone/trained_models/model_1.1.0/"
 
 accelerate launch train_text_to_image.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --train_data_dir=$TRAIN_DIR \
   --use_ema \
-  --resolution=512 --center_crop --random_flip \
-  --train_batch_size=1 \
-  --gradient_accumulation_steps=4 \
+  --resolution=512 \
+  --train_batch_size=8 \
+  --gradient_accumulation_steps=2 \
   --gradient_checkpointing \
   --mixed_precision="fp16" \
-  --max_train_steps=10 \
+  --max_train_steps=1500 \
   --learning_rate=1e-05 \
   --max_grad_norm=1 \
   --lr_scheduler="constant" --lr_warmup_steps=0 \
