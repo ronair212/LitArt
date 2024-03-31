9a0c4048ff91ef785c562e96054ea2ce26fc6102
adupatil <adwaitpatil1199@gmail.com>
2024-03-31 18:08:29 -0400

    front-end complete

--- application/app.py
+++ application/app.py
@@ -7,6 +7,9 @@ import time
 import requests
 from streamlit_lottie import st_lottie, st_lottie_spinner
 import json
+import PyPDF2
+import io
+import torch
 
 from scripts.inference import summarize,generate
 from LLama2.scripts.inference import generate_response
@@ -15,78 +18,97 @@ from utilities.helper_functions import text_to_prompt
 
 st.set_page_config(page_title="Book covers", page_icon=":notebook_with_decorative_cover:")
 processing_done = False
+image_generated = False
 
 st.sidebar.title("Navigation")    
 app_mode = st.sidebar.selectbox("Choose the app mode", ["Upload Chapters", "Generate Book Covers"])
-
 
 def load_lottie_file(path:str):
     with open(path) as f:
         data = json.load(f)
     return data
 
+@st.cache_data(show_spinner=False)
 def generate_text(temperature,chapter:str,sample:bool):
-    lottie = load_lottie_file("../utilities/Image_load.json")
-    with st_lottie_spinner(lottie,height=200):
-        summary_text = generate_response(chapter=chapter,
-                                model_dir="/work/LitArt/nair/outdir/meta-llama-Llama-2-7b-hf-2024-03-21-14:17:13",
-                                max_new_tokens = 500,
-                                do_sample = sample,
-                                temperature = float(temperature),
-                                top_p = 0.8,
-                                )
-        st.success("Summary created successfully!")
+    summary_text = generate_response(chapter=chapter,
+                            model_dir="/work/LitArt/nair/outdir/meta-llama-Llama-2-7b-hf-2024-03-21-14:17:13",
+                            max_new_tokens = 500,
+                            do_sample = sample,
+                            temperature = float(temperature),
+                            top_p = 0.8,
+                            )
+    st.success("Summary created successfully!")
+    torch.cuda.empty_cache()
     return summary_text
 
+@st.cache_data(show_spinner=False)
 def generate_image(prompt:str,book_title:str,adapter:str,num_covers:int=1):
-    lottie = load_lottie_file("../utilities/Image_gen.json")
-    with st_lottie_spinner(lottie,height=300):
-        for i in range(num_covers):
-            book_cover = generate(prompt=prompt,
-                model_name="CompVis/stable-diffusion-v1-4",
-                file_name=book_title+f"_{time.time()}",
-                lora=f"/work/LitArt/adwait/capstone/trained_adapters/{adapter}_1.1.0/")
+    covers = []
+    for i in range(num_covers):
+        book_cover = generate(prompt=prompt,
+            model_name="CompVis/stable-diffusion-v1-4",
+            file_name=book_title+f"_{time.time()}",
+            lora=f"/work/LitArt/adwait/capstone/trained_adapters/{adapter}_1.1.0/")
+        
+        covers.append(book_cover)
 
-        st.subheader("Generated Book Covers")
-        col1,col2 = st.columns(2)
-        with col1:
-            st.image("../../sample_outputs/Llama_dune_1711161535.376433.png", caption="Prompt for dune using the suspense adapter")
-        with col2:
-            st.image("../../sample_outputs/Llama_dune_1711161535.376433.png", caption="Prompt")
+    st.subheader("Generated Book Covers")
+    cols = st.columns(num_covers)
+
+    for i, image in enumerate(covers):
+        with cols[i]:
+            st.image(image, caption=prompt)
     
 
 if app_mode == "Upload Chapters":
     st.subheader("Upload Your Chapter Here")
     summarizer = st.selectbox('Choose the model to be used for summarization',['LLama','BART','T5','Seq2Seq'])
     if summarizer == 'LLama':
-        temprature = st.select_slider('Choose temprature',[0.5,0.6,0.7,0.8,0.9])
-        sample = st.selectbox('Sample',[True,False])
+        temprature = st.select_slider('Choose temprature',[0.5,0.6,0.7,0.8,0.9,1])
+        sample = st.selectbox('Sample',["True","False"])
 
     file_type = st.selectbox("Select File Type", ("pdf", "txt", "plain text"))
 
-    if file_type in ("pdf", "txt"):
-        uploaded_file = st.file_uploader("Choose a file")  
-        if uploaded_file is not None:
-            with open(uploaded_file) as f:
-                chapter = f.read()  
+    if file_type == "txt":
+        uploaded_file = st.file_uploader("Choose a file")
+        lottie = load_lottie_file("../utilities/anime.json")
+        if uploaded_file is not None:  
+            chapter = uploaded_file.getvalue().decode('utf-8')
+            st.write(chapter)
+            with st_lottie_spinner(lottie,height=200):
                 summary_text = generate_text(temperature=temprature,
-                                             chapter=chapter,
-                                             sample=sample)
-                st.text(f"{summary_text}")
-                processing_done = True 
-    
+                                            chapter=chapter,
+                                            sample=sample)
+            st.text(f"{summary_text}")
+            processing_done = True 
+
+    elif file_type == 'pdf':
+        uploaded_file = st.file_uploader("Choose a file")
+        lottie = load_lottie_file("../utilities/anime.json")
+        if uploaded_file is not None:
+            content = uploaded_file.getvalue()
+            pdf_reader = PyPDF2.PdfReader(io.BytesIO(content))
+            chapter = ''
+            
+            for page_num in range(pdf_reader.numPages):
+                page = pdf_reader.getPage(page_num)
+                chapter += page.extractText()
+
+            with st_lottie_spinner(lottie,height=200):
+                summary_text = generate_text(temperature=temprature,
+                                            chapter=chapter,
+                                            sample=sample)
+            st.text(f"{summary_text}")
+            processing_done = True 
+
     else:
         text_input = st.text_area("Write your text here")
-        if text_input:  
+        if text_input is not None:
             lottie = load_lottie_file("../utilities/anime.json")
             with st_lottie_spinner(lottie,height=200):
-                summary_text = generate_response(chapter=text_input,
-                                        model_dir="/work/LitArt/nair/outdir/meta-llama-Llama-2-7b-hf-2024-03-21-14:17:13",
-                                        max_new_tokens = 500,
-                                        do_sample = sample,
-                                        temperature = int(temperature),
-                                        top_p = 0.8,
-                                        )
+                summary_text = generate_text(temperature=temprature,
+                                        chapter=text_input,
+                                        sample=sample)
                 st.success("Summary created successfully!")
                 st.text(f"{summary_text}")
                 processing_done = True 
@@ -94,12 +116,46 @@ if app_mode == "Upload Chapters":
 
 if app_mode == "Generate Book Covers":
     st.subheader("Generate Book Covers")
-    st.text_area("Enter your prompt here keep it under 50 words ")
-    button_clicked = st.button("Generate Book Covers")
-    generate_image(prompt=summary_text)
+    lottie = load_lottie_file("../utilities/Image_gen.json")
 
-if processing_done:
+    adapter = st.selectbox("Select an adapter",['fiction','fantasy','suspense','speculative_fiction'])
+    title = st.text_input("Enter book title")
+    num_covers = st.select_slider("Number of Book covers",[1,2,3])
+    prompt = st.text_area("Enter your prompt here keep it under 30 words ")
+    prompt = text_to_prompt(text=prompt,model=adapter)
+
     button_clicked = st.button("Generate Book Covers")
 
     if button_clicked:
-        generate_image(prompt=summary_text)
+        with st_lottie_spinner(lottie,height=200):
+            generate_image(prompt=prompt,
+                    book_title=title,
+                    adapter=adapter,
+                    num_covers=int(num_covers))
+            
+        torch.cuda.empty_cache()
+        image_generated = True
+
+
+if processing_done :
+    adapter = st.selectbox("Select an adapter",['fiction','fantasy','suspense','speculative_fiction'])
+    title = st.text_input("Enter book title")
+    num_covers = st.select_slider("Number of Book covers",[1,2,3])
+    button_clicked = st.button("Generate Book Covers")
+    lottie = load_lottie_file("../utilities/Image_gen.json")
+    
+    if button_clicked:
+        prompt = text_to_prompt(text=summary_text,model=adapter)
+        with st_lottie_spinner(lottie,height=200):
+            generate_image(prompt=prompt,
+                    book_title=title,
+                    adapter=adapter,
+                    num_covers=int(num_covers))
+        
+        torch.cuda.empty_cache()
+        image_generated = True
+
+if image_generated:
+    clear_cache = st.button("clear cache")
+    if clear_cache:
+        st.cache_data.clear()
