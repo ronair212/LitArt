df75f7d2d6da977bfee2b501577e059db842db38
adupatil <adwaitpatil1199@gmail.com>
2024-04-07 21:48:12 -0400

    added max_tokens and inference_steps

--- application/app.py
+++ application/app.py
@@ -42,14 +42,14 @@ def load_lottie_file(path:str):
     return data
 
 @st.cache_data(show_spinner=False)
-def generate_text(chapter:str,sample:bool=False,temperature:int=1,summarizer:str="LLama"):
+def generate_text(chapter:str,sample:bool=False,temperature:int=1,summarizer:str="LLama",max_new_tokens:int=500):
 
     print(f"Summarizing using : {summarizer}")
     
     if summarizer=="LLama":
         summary_text = generate_response(chapter=chapter,
                             model_dir="/work/LitArt/nair/outdir/meta-llama-Llama-2-7b-hf-2024-03-21-14:17:13",
-                            max_new_tokens = 500,
+                            max_new_tokens = max_new_tokens,
                             do_sample = sample,
                             temperature = float(temperature),
                             top_p = 0.8,
@@ -62,13 +62,14 @@ def generate_text(chapter:str,sample:bool=False,temperature:int=1,summarizer:str
     return summary_text
 
 @st.cache_data(show_spinner=False)
-def generate_image(prompt:str,book_title:str,adapter:str,version:str,num_covers:int=1):
+def generate_image(prompt:str,book_title:str,adapter:str,version:str,num_covers:int=1,inference_steps:int=32):
     covers = []
     for i in range(num_covers):
         book_cover = generate(prompt=prompt,
             model_name="CompVis/stable-diffusion-v1-4",
             file_name=book_title+f"_{time.time()}",
-            lora=f"/work/LitArt/adwait/capstone/trained_adapters/{adapter}_{version}/")
+            lora=f"/work/LitArt/adwait/capstone/trained_adapters/{adapter}_{version}/",
+            inference_steps=inference_steps)
         
         covers.append(book_cover)
 
@@ -87,6 +88,7 @@ if app_mode == "Upload Chapters":
     summarizer = st.selectbox('Choose the model to be used for summarization',['LLama','BART','T5','Pegasus'])
 
     if summarizer == 'LLama':
+        max_tokens = st.text_input("Number of max tokens")
         temperature = st.select_slider('Choose temperature',[0.5,0.6,0.7,0.8,0.9,1])
         sample = st.selectbox('Sample',["True","False"])
 
@@ -103,7 +105,8 @@ if app_mode == "Upload Chapters":
                 summary_text = generate_text(temperature=temperature,
                                             chapter=chapter,
                                             summarizer=summarizer,
-                                            sample=sample)
+                                            sample=sample,
+                                            max_new_tokens=int(max_tokens))
             processing_done = True 
 
     elif file_type == 'pdf':
@@ -122,7 +125,8 @@ if app_mode == "Upload Chapters":
                 summary_text = generate_text(temperature=temperature,
                                             chapter=chapter,
                                             summarizer=summarizer,
-                                            sample=sample)
+                                            sample=sample,
+                                            max_new_tokens=int(max_tokens))
             processing_done = True 
 
     else:
@@ -133,7 +137,9 @@ if app_mode == "Upload Chapters":
                 summary_text = generate_text(temperature=temperature,
                                         chapter=text_input,
                                         summarizer=summarizer,
-                                        sample=sample)
+                                        sample=sample,
+                                        max_new_tokens=int(max_tokens))
+                                        
                 st.success("Summary created successfully!")
 
                 processing_done = True 
@@ -146,6 +152,7 @@ if app_mode == "Generate Book Covers":
     lottie = load_lottie_file("../utilities/Image_gen.json")
     adapter = st.selectbox("Select an adapter",['fiction','fantasy','suspense','speculative_fiction'])
     version = st.selectbox("Select adapter version",['1.1.0','2.1.0','3.0.1'])
+    inference_steps = st.text_input("Enter number of inference steps")
     title = st.text_input("Enter book title")
     num_covers = st.select_slider("Number of Book covers",[1,2,3])
     prompt = st.text_area("Enter your prompt here keep it under 30 words ")
@@ -159,7 +166,8 @@ if app_mode == "Generate Book Covers":
                     book_title=title,
                     adapter=adapter,
                     version=version,
-                    num_covers=int(num_covers))
+                    num_covers=int(num_covers),
+                    inference_steps=int(inference_steps))
             
         torch.cuda.empty_cache()
         image_generated = True
@@ -168,6 +176,7 @@ if app_mode == "Generate Book Covers":
 if processing_done :
     adapter = st.selectbox("Select an adapter",['fiction','fantasy','suspense','speculative_fiction'])
     version = st.selectbox("Select adapter version",['1.1.0','2.1.0','3.0.1'])
+    inference_steps = st.text_input("Enter number of inference steps")
     title = st.text_input("Enter book title")
     num_covers = st.select_slider("Number of Book covers",[1,2,3])
     button_clicked = st.button("Generate Book Covers")
@@ -180,7 +189,8 @@ if processing_done :
                     book_title=title,
                     adapter=adapter,
                     version=version,
-                    num_covers=int(num_covers))
+                    num_covers=int(num_covers),
+                    inference_steps=int(inference_steps))
         
         torch.cuda.empty_cache()
         image_generated = True
